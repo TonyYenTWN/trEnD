9aaf079a6b25419efcd46633e91e3c6765cf09df
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-05-14 03:03:16 +0200

    Update market_clearing.cpp

--- power_market/market_clearing.cpp
+++ power_market/market_clearing.cpp
@@ -19,7 +19,7 @@ struct network_graph{
 	// Power flow constrint
 	// 0th col: from start to end; 1st col: from end to start
 	Eigen::MatrixXd power_constraint;
-	
+
 	// Process variables
 	// Check if an edge is still available (flow constraint not reached)
 	Eigen::MatrixXi available_edge;
@@ -32,31 +32,35 @@ struct market_inform{
 	int time_intervals;
 	int price_intervals;
 	std::vector<std::string> zone_names;
-	Eigen::Vector2d price_range;
+	Eigen::Vector2d price_range_inflex;
+	Eigen::Vector2d price_range_flex;
+	Eigen::VectorXd bidded_price;
+	Eigen::MatrixXd merit_order_curve;
 	network_graph network;
 	
-	Eigen::VectorXd bidded_price;
-	Eigen::MatrixXd supply_curve;
-	Eigen::MatrixXd demand_curve;
+	// Process variables
+	Eigen::MatrixXd bidded_supply;
+	Eigen::MatrixXd bidded_demand;
 	
 	// Output results
 	Eigen::MatrixXd confirmed_supply;
 	Eigen::MatrixXd confirmed_demand;
-	Eigen::MatrixXd cleared_price;
+	Eigen::MatrixXd confirmed_price;
 	
 };
 
-int Time = 8760;
-market_inform International_Market;
-
 int main(){
+	int Time = 8760;
+	int prob_intervals = 100;
+	market_inform International_Market;
 	
 	// Parameters of international market
 	International_Market.num_zone = 13;
 	International_Market.time_intervals = Time;
 	International_Market.price_intervals = 600;
 	International_Market.zone_names = {"NO1","NO2","NO3","NO4","NO5","DE-LU","DK1","FI","GB","NL","SE1","SE2","SE3"};
-	International_Market.price_range << -500, 3000;
+	International_Market.price_range_inflex << -500, 3000;
+	International_Market.price_range_flex << -100, 500;
 	International_Market.network.num_vertices = International_Market.num_zone;
 	International_Market.network.num_edges = 15;
 	International_Market.network.incidence_matrix = Eigen::MatrixXi(International_Market.network.num_edges, 2);
@@ -68,24 +72,65 @@ int main(){
 	International_Market.network.available_edge = Eigen::MatrixXi::Zero(International_Market.network.num_edges, 2);
 
 	International_Market.bidded_price = Eigen::VectorXd(International_Market.price_intervals + 2);
-	International_Market.bidded_price(0) = International_Market.price_range(0);
-	International_Market.bidded_price.array().tail(1) = International_Market.price_range(1);
-	International_Market.bidded_price.array().segment(1, International_Market.price_intervals) = Eigen::VectorXd::LinSpaced(International_Market.price_intervals, -99.5, 499.5);
+	International_Market.bidded_price(0) = International_Market.price_range_inflex(0);
+	International_Market.bidded_price.array().tail(1) = International_Market.price_range_inflex(1);
+	International_Market.bidded_price.array().segment(1, International_Market.price_intervals) = Eigen::VectorXd::LinSpaced(International_Market.price_intervals, International_Market.price_range_flex(0) + .5, International_Market.price_range_flex(1) - .5);
 	
 	// Quantity density at each price
 	// Read infered merit order curve data
 	std::string fin_name;
-	int num_row = 101; 
+	int num_row = prob_intervals + 1; 
 	int num_col = International_Market.num_zone;
 	
 	// Read equiprobability price intervals
 	fin_name = "input/merit_order_curve_p.csv";
 	Eigen::MatrixXd merit_order_curve_p = read_file(num_row, num_col, fin_name);
+	Eigen::MatrixXd diff_merit_order_curve_p = merit_order_curve_p.bottomRows(num_row - 1) - merit_order_curve_p.topRows(num_row - 1);
 	
-	// Read quantity within each price interval
+	// Read quantity within each equiprobability price interval
 	fin_name = "input/merit_order_curve_q.csv";
 	Eigen::MatrixXd merit_order_curve_q = read_file(num_row, num_col, fin_name);
+	Eigen::MatrixXd diff_merit_order_curve_q = merit_order_curve_q.bottomRows(num_row - 1) - merit_order_curve_q.topRows(num_row - 1);
+	Eigen::MatrixXd slope_merit_order_curve_q = diff_merit_order_curve_q.array() * pow(diff_merit_order_curve_p.array(), -1);
 	
-	std::cout << merit_order_curve_q.topRows(5) << std::endl;
-
+	// Find quantity of supply at each equidistance price interval;
+	bool price_lower;
+	int start_interval;
+	int end_interval;
+	double slope_q_inflex;
+	International_Market.merit_order_curve = Eigen::MatrixXd::Zero(International_Market.price_intervals + 2, International_Market.num_zone);
+	
+	for(int zone_ID = 0; zone_ID < International_Market.num_zone; ++ zone_ID){ 
+		if(merit_order_curve_p(0, zone_ID) < International_Market.price_range_flex(0)){
+			price_lower = 1;
+		}
+		else{
+			price_lower = 0;
+			
+			slope_q_inflex = slope_merit_order_curve_q.col(zone_ID).minCoeff();
+			International_Market.merit_order_curve(0, zone_ID) = merit_order_curve_q(0, zone_ID) - (merit_order_curve_p(0, zone_ID) - International_Market.price_range_flex(0)) * slope_q_inflex;
+			
+			start_interval = 1;
+			end_interval = int (merit_order_curve_p(0, zone_ID) - International_Market.price_range_flex(0));
+			if(end_interval != 1){
+				International_Market.merit_order_curve.col(zone_ID).segment(start_interval, end_interval - start_interval + 1) = slope_q_inflex * Eigen::VectorXd::Ones(end_interval - start_interval + 1);
+			}
+		}
+		 
+		for(int prob_ID = 1; prob_ID < prob_intervals + 1; ++ prob_ID){
+			if(price_lower){
+				if(merit_order_curve_p(prob_ID, zone_ID) >= International_Market.price_range_flex(0)){
+					International_Market.merit_order_curve(0, zone_ID) += merit_order_curve_q(prob_ID - 1, zone_ID);
+					International_Market.merit_order_curve(0, zone_ID) += slope_merit_order_curve_q(prob_ID - 1, zone_ID) * (International_Market.price_range_flex(0) - merit_order_curve_p(prob_ID - 1, zone_ID));
+					price_lower = 0;
+					start_interval = 1;
+				}
+			}else{
+				
+			}
+		}
+	}
+	
+	std::cout << International_Market.merit_order_curve.topRows(200) << std::endl;
+	
 }
