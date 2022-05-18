b9dae28edc45ded02a3c6a31ce74b6f41cdd9ae2
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-05-18 19:56:13 +0200

    Update end-user.cpp

--- agent_operational/end-user.cpp
+++ agent_operational/end-user.cpp
@@ -17,12 +17,19 @@ struct end_user_operation{
 	double BESS_scale;
 	double EV_scale;
 	int EV_full_charge_duration;
-	double smart_appliance_scale;							// Indicates how much of the total demand in the time interval can be shifted around flexibly; assuming the default is constant profile before shifting
+	double smart_appliance_scale;							// Indicates how much ratio of the total demand in the time interval can be shifted around flexibly; assuming the default is constant profile before shifting
 	double smart_appliance_flexibility_factor;				// Indicates how flexible the smart appliances are; e.g. 1 / 2 = can concentrate the demand within half of the time interval
-	Eigen::VectorXd normalized_default_demand_profile;		// Normalized s.t. base value corresponds to meand demand at the spatial point
-	Eigen::VectorXd normalized_PV_profile;					// Normalized with the same base as demand
-	Eigen::VectorXi EV_usage_period;						// The time intervals when EV is actually used
-	Eigen::VectorXi EV_house_period;						// The time intervals when EV is parked in the house
+	Eigen::VectorXd normalized_default_demand_profile;		// Normalized to nominal value (kWh per hour per person)
+	Eigen::VectorXd normalized_default_PV_profile;			// Normalized with the same base as demand
+	Eigen::VectorXi EV_usage_default_period;				// The time intervals when EV is actually used
+	Eigen::VectorXi EV_house_default_period;				// The time intervals when EV is parked in the house
+	
+	// Output variables
+	Eigen::VectorXd normalized_scheduled_BESS_profile;
+	Eigen::VectorXd normalized_scheduled_EV_profile;
+	Eigen::VectorXd normalized_scheduled_smart_appliance_profile;
+	Eigen::VectorXd normalized_scheduled_pos_flex_profile;
+	Eigen::VectorXd normalized_scheduled_neg_flex_profile;
 };
 
 struct sorted_vector{
@@ -50,11 +57,31 @@ sorted_vector sort(Eigen::VectorXd original){
 
 int main(){
 	// Test case
-	Eigen::VectorXd subscept_tariff(10);
+	Eigen::VectorXd subscept_tariff(5);
 	subscept_tariff << 5, 6, 9, 2, 3;
 	sorted_vector sorted_tariff = sort(subscept_tariff);
 	
 	end_user_operation test_user;
 	test_user.normalized_default_demand_profile = Eigen::VectorXd(5);
 	test_user.normalized_default_demand_profile << 1, 1.2, 1.5, .8, .6;
+	test_user.smart_appliance_scale = .2;
+	test_user.smart_appliance_flexibility_factor = .5;
+	
+	// Flexible demand test
+	double test_total_flex_demand_energy = test_user.smart_appliance_scale * test_user.normalized_default_demand_profile.sum();
+	double test_max_flex_demand_capacity = test_total_flex_demand_energy / test_user.normalized_default_demand_profile.size() / test_user.smart_appliance_flexibility_factor;
+	int flex_demand_duration = int (test_user.smart_appliance_flexibility_factor * test_user.normalized_default_demand_profile.size());
+	if(double (test_user.smart_appliance_flexibility_factor * test_user.normalized_default_demand_profile.size() - flex_demand_duration) != 0){
+		flex_demand_duration += 1;
+	}
+	test_user.normalized_scheduled_smart_appliance_profile = Eigen::VectorXd::Zero(test_user.normalized_default_demand_profile.size());
+	for(int tock = 0; tock < flex_demand_duration - 1; ++ tock){
+		test_user.normalized_scheduled_smart_appliance_profile(sorted_tariff.id(tock)) = test_max_flex_demand_capacity;
+	}
+	test_user.normalized_scheduled_smart_appliance_profile(sorted_tariff.id(flex_demand_duration - 1)) = test_total_flex_demand_energy - (flex_demand_duration - 1) * test_max_flex_demand_capacity;
+	
+//	std::cout << test_total_flex_demand_energy << " " << test_max_flex_demand_capacity << " " << flex_demand_duration << std::endl;
+//	std::cout << subscept_tariff.transpose() << std::endl;
+	std::cout << test_user.normalized_scheduled_smart_appliance_profile.transpose() << std::endl;
+	std::cout << test_user.normalized_scheduled_smart_appliance_profile.transpose() + (1 - test_user.smart_appliance_scale) * test_user.normalized_default_demand_profile.transpose() << std::endl;
 }
