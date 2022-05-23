2df7dbef6eab73d07e3de40f5f64367a3777b58c
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-05-23 15:17:31 +0200

    Update end-user.cpp

--- agent_operational/end-user.cpp
+++ agent_operational/end-user.cpp
@@ -5,6 +5,19 @@
 //#include <boost/math/distributions/normal.hpp>
 #include "../basic/Basic_Definitions.h"
 
+struct storage_inform{
+	// Input parameters
+	double energy_scale;									// kWh per person
+	double capacity_scale;									// kW per person
+	double efficiency;
+	double soc_ini;
+	double soc_final;
+	
+	// Output variables
+	Eigen::VectorXd normalized_scheduled_capacity_profile;
+	Eigen::VectorXd normalized_scheduled_soc_profile;
+};
+
 struct end_user_operation{
 	// Input parameters
 	int point_ID;
@@ -15,9 +28,6 @@ struct end_user_operation{
 	bool reverse_flow;										// Whether the end-user can inject power flow back to grid; false when active_flow is false
 	bool active_flex;										// Whether the end-user can provide flexibility to the aggregator; false when dynamic_tariff is false, or when end-user does not have PV + BESS, EV, or smart appliance
 	double PV_scale;										// Unless mentioned otherwise, all the scaling factor = capacity / normalized default base value
-	double BESS_energy_scale;								// kWh per person
-	double BESS_capacity_scale;
-	double BESS_efficiency;
 	double EV_energy_scale;
 	double EV_capacity_scale;
 	double EV_efficiency;
@@ -30,13 +40,14 @@ struct end_user_operation{
 	
 	// Output variables
 	Eigen::VectorXd normalized_scheduled_residual_demand_inflex_profile;
-	Eigen::VectorXd normalized_scheduled_BESS_capacity_profile;
-	Eigen::VectorXd normalized_scheduled_BESS_energy_profile;
 	Eigen::VectorXd normalized_scheduled_EV_capcity_profile;
 	Eigen::VectorXd normalized_scheduled_EV_energy_profile;
 	Eigen::VectorXd normalized_scheduled_smart_appliance_profile;
 	Eigen::VectorXd normalized_scheduled_pos_cr_profile;
 	Eigen::VectorXd normalized_scheduled_neg_cr_profile;
+	
+	// Mixed Substructure
+	storage_inform BESS;
 };
 
 struct sorted_vector{
@@ -57,6 +68,38 @@ sorted_vector sort(Eigen::VectorXd original){
  	for(int item_ID = 0; item_ID < original.size(); ++ item_ID){
  		result.id(item_ID) = item_seq[item_ID];
  		result.value(item_ID) = original(item_seq[item_ID]);
+	}
+	
+	return(result);
+}
+
+storage_inform storage_schedule(sorted_vector sorted_tariff, storage_inform input_inform){
+	// Initialization
+	storage_inform result = input_inform;
+	int total_duration = sorted_tariff.value.size();
+	int ch_surplus_duration = int(std::max(result.soc_final - result.soc_ini, 0.) / result.capacity_scale / result.efficiency);
+	int dc_surplus_duration = int(std::max(result.soc_ini - result.soc_final, 0.) / result.capacity_scale * result.efficiency);
+	result.normalized_scheduled_capacity_profile = Eigen::VectorXd::Zero(total_duration);
+	result.normalized_scheduled_soc_profile = result.soc_ini * Eigen::VectorXd::Ones(total_duration);
+	//result.normalized_scheduled_soc_profile(0) = result.soc_ini;
+	//result.normalized_scheduled_soc_profile(total_duration - 1) = result.soc_final;
+	
+	// Schedule of surplus dc / ch so that the soc change is as expected
+	if(ch_surplus_duration > 0){
+		for(int tick = 0; tick < ch_surplus_duration; ++ tick){
+			result.normalized_scheduled_capacity_profile(sorted_tariff.id(tick)) = -result.capacity_scale;
+			for(int tock = sorted_tariff.id(tick); tock < total_duration; ++ tock){
+				result.normalized_scheduled_soc_profile(tock) += result.capacity_scale * result.efficiency;
+			}
+		}
+	}
+	else{
+		for(int tick = 0; tick < dc_surplus_duration; ++ tick){
+			result.normalized_scheduled_capacity_profile(sorted_tariff.id(total_duration - tick - 1)) = result.capacity_scale;
+			for(int tock = sorted_tariff.id(total_duration - tick - 1); tock < total_duration; ++ tock){
+				result.normalized_scheduled_soc_profile(tock) -= result.capacity_scale / result.efficiency;
+			}
+		}
 	}
 	
 	return(result);
@@ -87,28 +130,35 @@ int main(){
 	}
 	test_user.normalized_scheduled_smart_appliance_profile(sorted_tariff.id(flex_demand_duration - 1)) = test_total_flex_demand_energy - (flex_demand_duration - 1) * test_max_flex_demand_capacity;
 	
-	std::cout << test_user.normalized_scheduled_smart_appliance_profile.transpose() << std::endl;
-	std::cout << test_user.normalized_scheduled_smart_appliance_profile.transpose() + (1 - test_user.smart_appliance_scale) * test_user.normalized_default_demand_profile.transpose() << std::endl;
+//	std::cout << test_user.normalized_scheduled_smart_appliance_profile.transpose() << std::endl;
+//	std::cout << test_user.normalized_scheduled_smart_appliance_profile.transpose() + (1 - test_user.smart_appliance_scale) * test_user.normalized_default_demand_profile.transpose() << std::endl;
 	
 	// BESS test, naive
-	test_user.BESS_energy_scale = 4;
-	test_user.BESS_capacity_scale = 1;
-	test_user.BESS_efficiency = .95;
-	test_user.normalized_scheduled_BESS_energy_profile = Eigen::VectorXd::Zero(subscept_tariff.size());
-	// Need to determine best end storage level (given start storage level) in the future
-	test_user.normalized_scheduled_BESS_energy_profile(0) = test_user.BESS_energy_scale;
-	test_user.normalized_scheduled_BESS_energy_profile(subscept_tariff.size() - 1) = test_user.BESS_energy_scale;
-	int BESS_ch_duration = int ((test_user.BESS_energy_scale - (std::max(test_user.normalized_scheduled_BESS_energy_profile(0) - test_user.normalized_scheduled_BESS_energy_profile(subscept_tariff.size() - 1), 0.))) / test_user.BESS_capacity_scale);
-	int BESS_dc_duration = int ((test_user.BESS_energy_scale - (std::max(test_user.normalized_scheduled_BESS_energy_profile(subscept_tariff.size() - 1) - test_user.normalized_scheduled_BESS_energy_profile(0), 0.))) / test_user.BESS_capacity_scale);
-	//std::cout << BESS_ch_duration << " " << BESS_dc_duration << std::endl;
-	
-	for(int tock = 0; tock < BESS_ch_duration; ++ tock){
-		test_user.normalized_scheduled_BESS_energy_profile(sorted_tariff.id(tock)) = -test_user.BESS_capacity_scale / test_user.BESS_efficiency;
-	}
-	for(int tock = 0; tock < BESS_dc_duration; ++ tock){
-		test_user.normalized_scheduled_BESS_energy_profile(sorted_tariff.id(sorted_tariff.id.size() - tock)) = test_user.BESS_capacity_scale * test_user.BESS_efficiency;
-	}
-	std::cout << test_user.normalized_scheduled_BESS_energy_profile.transpose() << std::endl;
+	test_user.BESS.energy_scale = 4;
+	test_user.BESS.capacity_scale = 1;
+	test_user.BESS.efficiency = .95;
+	test_user.BESS.soc_ini = 2;
+	test_user.BESS.soc_final = 2;
+	test_user.BESS = storage_schedule(sorted_tariff, test_user.BESS);
+	std::cout << test_user.BESS.normalized_scheduled_soc_profile.transpose() << "\n" << std::endl;
+//	test_user.BESS_energy_scale = 4;
+//	test_user.BESS_capacity_scale = 1;
+//	test_user.BESS_efficiency = .95;
+//	test_user.normalized_scheduled_BESS_energy_profile = Eigen::VectorXd::Zero(subscept_tariff.size());
+//	// Need to determine best end storage level (given start storage level) in the future
+//	test_user.normalized_scheduled_BESS_energy_profile(0) = test_user.BESS_energy_scale;
+//	test_user.normalized_scheduled_BESS_energy_profile(subscept_tariff.size() - 1) = test_user.BESS_energy_scale;
+//	int BESS_ch_duration = int ((test_user.BESS_energy_scale - (std::max(test_user.normalized_scheduled_BESS_energy_profile(0) - test_user.normalized_scheduled_BESS_energy_profile(subscept_tariff.size() - 1), 0.))) / test_user.BESS_capacity_scale);
+//	int BESS_dc_duration = int ((test_user.BESS_energy_scale - (std::max(test_user.normalized_scheduled_BESS_energy_profile(subscept_tariff.size() - 1) - test_user.normalized_scheduled_BESS_energy_profile(0), 0.))) / test_user.BESS_capacity_scale);
+//	//std::cout << BESS_ch_duration << " " << BESS_dc_duration << std::endl;
+//	
+//	for(int tock = 0; tock < BESS_ch_duration; ++ tock){
+//		test_user.normalized_scheduled_BESS_energy_profile(sorted_tariff.id(tock)) = -test_user.BESS_capacity_scale / test_user.BESS_efficiency;
+//	}
+//	for(int tock = 0; tock < BESS_dc_duration; ++ tock){
+//		test_user.normalized_scheduled_BESS_energy_profile(sorted_tariff.id(sorted_tariff.id.size() - tock)) = test_user.BESS_capacity_scale * test_user.BESS_efficiency;
+//	}
+//	std::cout << test_user.normalized_scheduled_BESS_energy_profile.transpose() << std::endl;
 	
 	// EV test
 	int EV_activity_duration = 0;
