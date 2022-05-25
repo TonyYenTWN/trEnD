d97fde7493ed0bba9bed928ee6fd31a87622b37c
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-05-25 05:24:32 +0200

    Update market_clearing.cpp

--- power_market/market_clearing.cpp
+++ power_market/market_clearing.cpp
@@ -1,11 +1,8 @@
 // Main Source File
 #include <iostream>
-#include <chrono>
+//#include <chrono>
 #include <omp.h>
-#include <boost/math/distributions/normal.hpp>
 #include "../basic/rw_csv.cpp"
-
-boost::math::normal norm_dist(0.0, 1.0);
 
 struct network_graph{
 	// Input parameters
