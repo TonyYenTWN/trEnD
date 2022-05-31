dab25ce5fc439c0cef736d8c5464fe6ab704c856
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-05-31 15:44:53 +0200

    Create Eigen_Sparse.h

---
+++ basic/Eigen_Sparse.h
@@ -0,0 +1,8 @@
+#ifndef EIGEN_SPARSE
+#define EIGEN_SPARSE
+#include <Eigen/Sparse>
+#include <Eigen/SparseLU>
+#include <Eigen/SparseQR>
+#include <Eigen/SparseCholesky>							// Use only when the matric is symmetric positive definite (ex. covariance or laplacian matrix)
+typedef Eigen::Triplet <double> Trip;    				// Define a triplet object
+#endif
