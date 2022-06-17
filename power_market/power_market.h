a0608332da38ec195c4c66e9b36a697b3fed7a46
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-06-17 13:35:49 +0200

    Update power_market.h

--- power_market/power_market.h
+++ power_market/power_market.h
@@ -15,7 +15,6 @@ struct network_graph{
 	int num_edges;
 	Eigen::MatrixXi incidence_matrix;		// Compact Incidence Matrix (0th col: start; 1st col: end)
 	Eigen::VectorXd admittance_vector;		// (Imaginary part of) admittance of each edge; used in TSO and DSO
-//	Eigen::SparseMatrix <double> Y_n;		// Node admittance matrix; used in TSO and DSO
 	Eigen::MatrixXd voltage_constraint;		// Voltage constraint at each node; used in TSO and DSO
 	Eigen::MatrixXd power_constraint;  		// Power flow constraint (0th col: from start to end; 1st col: from end to start)
 
@@ -36,18 +35,18 @@ struct market_inform{
 	Eigen::VectorXd bidded_price;
 	Eigen::MatrixXd merit_order_curve;
 	Eigen::MatrixXd demand_default;			// Default demand profiles of the bidding zones; in later runs demand bids from Norway should come from lower level markets
-	
+
 	// Process Variables
 	Eigen::MatrixXd submitted_supply;		// Supply bid submitted in the bidding zones
 	Eigen::MatrixXd submitted_demand;		// Demand bid submitted in the bidding zones
-	
+
 	// Output variables
-	Eigen::MatrixXd confirmed_supply;		// Confirmed supply quantity from MO or TSO 
-	Eigen::MatrixXd confirmed_demand;		// Confirmed demand quantity from MO or TSO 
-	Eigen::MatrixXd confirmed_price;		// Confirmed market clearing price from MO or TSO 
-	//Eigen::MatrixXd confirmed_bid_supply; 	// Confirmed supply bids from DSOs forwarding to wholesale electricity market
+	Eigen::MatrixXd confirmed_supply;		// Confirmed supply quantity from MO or TSO
+	Eigen::MatrixXd confirmed_demand;		// Confirmed demand quantity from MO or TSO
+	Eigen::MatrixXd confirmed_price;		// Confirmed market clearing price from MO or TSO
+	//Eigen::MatrixXd confirmed_bid_supply; // Confirmed supply bids from DSOs forwarding to wholesale electricity market
 	//Eigen::MatrixXd confirmed_bid_demand;	// Confirmed demand bids from DSOs forwarding to wholesale electricity market
-	
+
 	// Mixed Substructure
 	network_graph network;
 };
