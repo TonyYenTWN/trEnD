7d9667483933a2e5900ee41ec667f3fe4bdda063
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-07-09 21:37:10 +0200

    Update Basic_Definitions.h

--- basic/Basic_Definitions.h
+++ basic/Basic_Definitions.h
@@ -23,9 +23,13 @@
 #ifndef EIGEN
 #define EIGEN
 #include <Eigen/Dense>
+#include <Eigen/Core>
+namespace Eigen{
+	typedef Eigen::Matrix<ptrdiff_t, Eigen::Dynamic, 1> VectorXpd;
+}
 #endif
 
-#ifndef PI
-#define PI
-#define pi 3.141592653589793238462643383279502884  /* pi */
+#ifndef CONSTANT
+#define CONSTANT
+#include <boost/math/constants/constants.hpp>
 #endif
