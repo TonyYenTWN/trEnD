c175a1de774abab53a256b6910647910c4e52480
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-07-12 17:56:46 +0200

    update test run files

--- test_run/TSO_test.cpp
+++ test_run/TSO_test.cpp
@@ -7,13 +7,13 @@
 #include "../power_network/power_network.h"
 #include "../power_market/power_market.h"
 
-void TSO_Market_Set_Test_1(market_inform &TSO_Market, int Time){	
+void TSO_Market_Set_Test_1(market_inform &TSO_Market, int Time){
 	// Input parameters of TSO market
 	// A trivial test case with 3 nodes, the first bus being the reference connecting to the remaining 2
 	TSO_Market.num_zone = 3;
 	TSO_Market.time_intervals = Time;
 	TSO_Market.set_bidded_price();
-	
+
 	// Set compact node admittance matrix Y_n
 	TSO_Market.network.num_vertice = TSO_Market.num_zone;
 	TSO_Market.network.num_edges = 3;
@@ -23,7 +23,7 @@ void TSO_Market_Set_Test_1(market_inform &TSO_Market, int Time){
 	TSO_Market.network.incidence_matrix.row(2) << 1, 2;
 	TSO_Market.network.admittance_vector = Eigen::VectorXd(3);
 	TSO_Market.network.admittance_vector << 100., 100., 100.;
-	
+
 	// Set voltage and power constraints at each edge
 	TSO_Market.network.voltage_constraint = Eigen::MatrixXd(TSO_Market.network.num_vertice, 2);
 	TSO_Market.network.voltage_constraint.col(0) = Eigen::VectorXd::Constant(TSO_Market.network.num_vertice, -.2);
@@ -36,8 +36,8 @@ void TSO_Market_Set_Test_1(market_inform &TSO_Market, int Time){
 	TSO_Market.confirmed_supply = Eigen::MatrixXd::Zero(Time, TSO_Market.num_zone);
 	TSO_Market.confirmed_demand = Eigen::MatrixXd::Zero(Time, TSO_Market.num_zone);
 	TSO_Market.confirmed_price = Eigen::MatrixXd(Time, TSO_Market.num_zone);
-	TSO_Market.network.confirmed_power = Eigen::MatrixXd(Time, TSO_Market.network.num_edges);	
-	
+	TSO_Market.network.confirmed_power = Eigen::MatrixXd(Time, TSO_Market.network.num_edges);
+
 	// For the trivial case only: initialize submitted supply and demand bids at each node
 	Market_Initialization(TSO_Market);
 	TSO_Market.submitted_supply(0, 0) = 10.;
@@ -58,10 +58,10 @@ void TSO_Market_Set_Test_1(market_inform &TSO_Market, int Time){
 void TSO_Market_Set_Test_2(market_inform &TSO_Market, int Time){
 	// Input parameters of TSO market
 	// A trivial test case with 20 nodes connected as a radial line
-	TSO_Market.num_zone = 2;
+	TSO_Market.num_zone = 5000;
 	TSO_Market.time_intervals = Time;
 	TSO_Market.set_bidded_price();
-	
+
 	// Set node admittance matrix Y_n
 	TSO_Market.network.num_vertice = TSO_Market.num_zone;
 	TSO_Market.network.num_edges = TSO_Market.num_zone - 1;
@@ -70,7 +70,7 @@ void TSO_Market_Set_Test_2(market_inform &TSO_Market, int Time){
 		TSO_Market.network.incidence_matrix.row(edge_iter) << edge_iter, edge_iter + 1;
 	}
 	TSO_Market.network.admittance_vector = Eigen::VectorXd::Constant(TSO_Market.network.num_edges, 5000.);
-	
+
 	// Set voltage and power constraints at each edge
 	TSO_Market.network.voltage_constraint = Eigen::MatrixXd(TSO_Market.network.num_vertice, 2);
 	TSO_Market.network.voltage_constraint.col(0) = Eigen::VectorXd::Constant(TSO_Market.network.num_vertice, -.1);
@@ -84,7 +84,7 @@ void TSO_Market_Set_Test_2(market_inform &TSO_Market, int Time){
 	TSO_Market.confirmed_demand = Eigen::MatrixXd::Zero(Time, TSO_Market.num_zone);
 	TSO_Market.confirmed_price = Eigen::MatrixXd(Time, TSO_Market.num_zone);
 	TSO_Market.network.confirmed_power = Eigen::MatrixXd(Time, TSO_Market.network.num_edges);
-	
+
 	// For the trivial case only: initialize submitted supply and demand bids at each node
 	Market_Initialization(TSO_Market);
 	TSO_Market.submitted_supply.leftCols(TSO_Market.num_zone / 2) = Eigen::MatrixXd::Constant(TSO_Market.price_intervals + 2, TSO_Market.num_zone / 2, 1.);
@@ -93,8 +93,8 @@ void TSO_Market_Set_Test_2(market_inform &TSO_Market, int Time){
 
 int main(){
 	network_inform Power_network_inform;
-	power_network_input_process(Power_network_inform, "../power_network/");	
-	
+	power_network_input_process(Power_network_inform, "../power_network/");
+
 	market_inform TSO_Market;
 	TSO_Market_Set_Test_2(TSO_Market, 1);
 	alglib::minlpstate TSO_Problem;
@@ -111,8 +111,8 @@ int main(){
 	stop = std::chrono::high_resolution_clock::now();
 	duration = std::chrono::duration_cast <std::chrono::microseconds> (stop - start);
 	std::cout << "Optimization time: " << duration.count() << " microseconds" << "\n\n";
-	
-	// Check issues of memory allocation 
+
+	// Check issues of memory allocation
 	TSO_Market_Set_Test_1(TSO_Market, 1);
 
 	start = std::chrono::high_resolution_clock::now();
@@ -126,5 +126,5 @@ int main(){
 	//Flow_Based_Market_Optimization_Test(0, TSO_Market, TSO_Problem);
 	stop = std::chrono::high_resolution_clock::now();
 	duration = std::chrono::duration_cast <std::chrono::microseconds> (stop - start);
-	std::cout << "Optimization time: " << duration.count() << " microseconds" << "\n\n";	
+	std::cout << "Optimization time: " << duration.count() << " microseconds" << "\n\n";
 }
