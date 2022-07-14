75338b7844369da5375161323d1f3f66a0662f83
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-07-14 11:01:51 +0200

    Update power_network.h

--- src/power_network/power_network.h
+++ src/power_network/power_network.h
@@ -79,11 +79,11 @@ namespace power_network{
 		double line_density_distr;
 		double fraction_dim_distr = 1.5;
 
-        std::complex<double> z_trans_series = std::complex<double> (0., 5. * pow(10., -4.));	/**< Series impedence per meter of transmission line */
-        std::complex<double> z_trans_shunt = std::complex<double> (0., 0.);							/**< Shunt impedence per meter of transmission line */
-        std::complex<double> z_distr_series = std::complex<double> (0., 7. * pow(10., -4.));	/**< Series impedence per meter of distribution line */
-        std::complex<double> z_distr_shunt = std::complex<double> (0., 0.);							/**< Shunt impedence per meter of distribution line */
-		std::complex<double> s_base = std::complex<double> (1000., 0.) * pow(3., .5);			/**< Reference value for non-dimensionalization of power into p.u. */
+		std::complex<double> z_trans_series = std::complex<double> (0., 5. * pow(10., -4.));	/**< Series impedence per meter of transmission line */
+		std::complex<double> z_trans_shunt = std::complex<double> (0., 0.);	/**< Shunt impedence per meter of transmission line */
+		std::complex<double> z_distr_series = std::complex<double> (0., 7. * pow(10., -4.));	/**< Series impedence per meter of distribution line */
+		std::complex<double> z_distr_shunt = std::complex<double> (0., 0.);	/**< Shunt impedence per meter of distribution line */
+		std::complex<double> s_base = std::complex<double> (1000., 0.) * pow(3., .5);	/**< Reference value for non-dimensionalization of power into p.u. */
     };
 
 	struct network_inform{
