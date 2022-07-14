c8f11f08b40b7c62eb92bb3509e2b95f508a5cc7
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-07-14 10:29:32 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,27 +17,36 @@ int main(){
 	int Time = 8760;
 	std::string fin_name_moc = "csv/input/power_market/merit_order_curve_q_assimilated_2021.csv";
 	std::string fin_name_demand = "csv/input/power_market/residual_load_default_forecast_2021.csv";
-	market_inform International_Market;
-	International_Market_Set(International_Market, Time, fin_name_moc, fin_name_demand);
+	power_market::market_inform International_Market;
+	power_market::International_Market_Set(International_Market, Time, fin_name_moc, fin_name_demand);
 
 	// Initialization of the TSO
-	market_inform TSO_Market;
+	power_market::market_inform TSO_Market;
+	power_market::TSO_Market_Set(TSO_Market, Power_network_inform, Time);
 	alglib::minlpstate TSO_Problem;
-	TSO_Market_Set(TSO_Market, Power_network_inform, Time);
-	Flow_Based_Market_LP_Set(TSO_Market, TSO_Problem);
+	power_market::Flow_Based_Market_LP_Set(TSO_Market, TSO_Problem);
 
 	// Initialization of the DSO
-	DSO_Markets DSO_Markets;
-	DSO_Markets_Set(DSO_Markets, Power_network_inform, Time);
+	power_market::DSO_Markets DSO_Markets;
+	power_market::DSO_Markets_Set(DSO_Markets, Power_network_inform, Time);
+	alglib::minlpstate DSO_Problems;//[DSO_Markets.markets.size()];
+	power_market::Flow_Based_Market_LP_Set(DSO_Markets.markets[6], DSO_Problems);
+	// create a minlpstate object in market_inform structure to avoid collision
+//	//for(int DSO_iter = 0; DSO_iter < DSO_Markets.markets.size(); ++ DSO_iter){
+//	for(int DSO_iter = 0; DSO_iter < 10; ++ DSO_iter){
+//		if(DSO_Markets.markets[DSO_iter].num_zone > 1){
+//		}
+//	}
 
 	// Re-initialization of submitted bids
 	std::string fin_point_demand = "csv/processed/spatial_field/nominal_mean_demand_field_10km_annual_mean.csv";
-	Submitted_bid_calculation(0, DSO_Markets, TSO_Market, International_Market, Power_network_inform, fin_point_demand);
+	power_market::Submitted_bid_calculation(0, DSO_Markets, TSO_Market, International_Market, Power_network_inform, fin_point_demand);
 
 	// Ideal market clearing in IMO
 
 	// Bid-filtering in DSOs
+	//Flow_Based_Market_Optimization(0, DSO_Markets.markets[6], DSO_Problems);
 
-	// Re-dispatch in TSO
-	Flow_Based_Market_Optimization(0, TSO_Market, TSO_Problem);
+	// Re-dispatch + tertiary control reserve in TSO
+	power_market::Flow_Based_Market_Optimization(0, TSO_Market, TSO_Problem);
 }
