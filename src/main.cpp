1e00a2cbb2ea92b9829aa7d1e6dd48cc6cb86fe5
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-07-14 19:13:00 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,20 +23,18 @@ int main(){
 	// Initialization of the TSO
 	power_market::market_inform TSO_Market;
 	power_market::TSO_Market_Set(TSO_Market, Power_network_inform, Time);
-	alglib::minlpstate TSO_Problem;
-	power_market::Flow_Based_Market_LP_Set(TSO_Market, TSO_Problem);
+	power_market::Flow_Based_Market_LP_Set(TSO_Market);
 
 	// Initialization of the DSO
-	power_market::DSO_Markets DSO_Markets;
+	power_market::markets_inform DSO_Markets;
 	power_market::DSO_Markets_Set(DSO_Markets, Power_network_inform, Time);
-	alglib::minlpstate DSO_Problems;//[DSO_Markets.markets.size()];
-	power_market::Flow_Based_Market_LP_Set(DSO_Markets.markets[6], DSO_Problems);
-	// create a minlpstate object in market_inform structure to avoid collision
-//	//for(int DSO_iter = 0; DSO_iter < DSO_Markets.markets.size(); ++ DSO_iter){
-//	for(int DSO_iter = 0; DSO_iter < 10; ++ DSO_iter){
-//		if(DSO_Markets.markets[DSO_iter].num_zone > 1){
+	//for(int DSO_iter = 0; DSO_iter < DSO_Markets.size(); ++ DSO_iter){
+	for(int DSO_iter = 0; DSO_iter < 7; ++ DSO_iter){
+//		if(DSO_Markets[DSO_iter].num_zone > 1){
+//			power_market::Flow_Based_Market_LP_Set(DSO_Markets[DSO_iter]);
 //		}
-//	}
+	}
+	std::cout << DSO_Markets[6].num_zone << "\n\n";
 
 	// Re-initialization of submitted bids
 	std::string fin_point_demand = "csv/processed/spatial_field/nominal_mean_demand_field_10km_annual_mean.csv";
@@ -48,5 +46,5 @@ int main(){
 	//Flow_Based_Market_Optimization(0, DSO_Markets.markets[6], DSO_Problems);
 
 	// Re-dispatch + tertiary control reserve in TSO
-	power_market::Flow_Based_Market_Optimization(0, TSO_Market, TSO_Problem);
+	power_market::Flow_Based_Market_Optimization(0, TSO_Market);
 }
