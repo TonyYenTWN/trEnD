3884c3f4d9729b29c8e1c3993224cfdb73329dc3
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-07-14 11:32:32 +0200

    Update DSO.cpp

--- src/power_market/DSO.cpp
+++ src/power_market/DSO.cpp
@@ -1,4 +1,5 @@
 // Source file for dispatch filtering of DSOs in Norway
+// Note that in Norwegian terminology, HV distribution network (> 33kV) is called regional network.
 #include <iostream>
 //#include <chrono>
 //#include "../basic/LP_gpa.h"
