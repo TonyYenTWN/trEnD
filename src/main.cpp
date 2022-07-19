213d889ba02978ef85ae8b944fc2aa3666488738
Tony Yen <tony.yen.uni.freiburg@gmail.com>
2022-07-19 08:51:04 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,30 +20,30 @@ int main(){
 	power_network::network_inform Power_network_inform;
 	power_network::power_network_input_process(Power_network_inform, "csv/input/power_network/");
 
-//	// Initialization of the IMO
-//	int Time = 8760;
-//	std::string fin_name_moc = "csv/input/power_market/merit_order_curve_q_assimilated_2021.csv";
-//	std::string fin_name_demand = "csv/input/power_market/residual_load_default_forecast_2021.csv";
-//	power_market::market_inform International_Market;
-//	power_market::International_Market_Set(International_Market, Time, fin_name_moc, fin_name_demand);
-//
-//	// Initialization of the TSO
-//	power_market::market_inform TSO_Market;
-//	power_market::TSO_Market_Set(TSO_Market, Power_network_inform, Time);
-//	alglib::minlpstate TSO_Problem;
-//	power_market::Flow_Based_Market_LP_Set(TSO_Market, TSO_Problem);
-//
-//	// Initialization of the DSO
-//	power_market::markets_inform DSO_Markets;
-//	power_market::DSO_Markets_Set(DSO_Markets, Power_network_inform, Time);
+	// Initialization of the IMO
+	int Time = 8760;
+	std::string fin_name_moc = "csv/input/power_market/merit_order_curve_q_assimilated_2021.csv";
+	std::string fin_name_demand = "csv/input/power_market/residual_load_default_forecast_2021.csv";
+	power_market::market_inform International_Market;
+	power_market::International_Market_Set(International_Market, Time, fin_name_moc, fin_name_demand);
+
+	// Initialization of the TSO
+	power_market::market_inform TSO_Market;
+	power_market::TSO_Market_Set(TSO_Market, Power_network_inform, Time);
+	alglib::minlpstate TSO_Problem;
+	power_market::Flow_Based_Market_LP_Set(TSO_Market, TSO_Problem);
+
+	// Initialization of the DSO
+	power_market::markets_inform DSO_Markets;
+	power_market::DSO_Markets_Set(DSO_Markets, Power_network_inform, Time);
 //	std::vector <alglib::minlpstate> DSO_Problems (DSO_Markets.size());
 //	for(int DSO_iter = 0; DSO_iter < DSO_Markets.size(); ++ DSO_iter){
 //		power_market::Flow_Based_Market_LP_Set(DSO_Markets[DSO_iter], DSO_Problems[DSO_iter]);
 //	}
 //
-//	// Re-initialization of submitted bids
-//	std::string fin_point_demand = "csv/processed/spatial_field/nominal_mean_demand_field_10km_annual_mean.csv";
-//	power_market::Submitted_bid_calculation(0, DSO_Markets, TSO_Market, International_Market, Power_network_inform, fin_point_demand);
+	// Re-initialization of submitted bids
+	std::string fin_point_demand = "csv/processed/spatial_field/nominal_mean_demand_field_10km_annual_mean.csv";
+	power_market::Submitted_bid_calculation(0, DSO_Markets, TSO_Market, International_Market, Power_network_inform, fin_point_demand);
 //
 //	stop = std::chrono::high_resolution_clock::now();
 //	duration = std::chrono::duration_cast <std::chrono::microseconds> (stop - start);
@@ -63,14 +63,14 @@ int main(){
 ////		std::cout << DSO_iter << "-th DSO optimization time: " << duration.count() << " microseconds" << "\n";
 ////		std::cout << "--------------------------------------------------------------------------------------------------------------\n\n";
 ////	}
-//
-//	// Re-dispatch + tertiary control reserve in TSO
-//	start = std::chrono::high_resolution_clock::now();
-//
-//	power_market::Flow_Based_Market_Optimization(0, TSO_Market, TSO_Problem);
-//
-//	stop = std::chrono::high_resolution_clock::now();
-//	duration = std::chrono::duration_cast <std::chrono::microseconds> (stop - start);
-//	std::cout << "TSO optimization time: " << duration.count() << " microseconds" << "\n\n";
+
+	// Re-dispatch + tertiary control reserve in TSO
+	start = std::chrono::high_resolution_clock::now();
+
+	power_market::Flow_Based_Market_Optimization(0, TSO_Market, TSO_Problem);
+
+	stop = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast <std::chrono::microseconds> (stop - start);
+	std::cout << "TSO optimization time: " << duration.count() << " microseconds" << "\n\n";
 //	//std::cin.get();
 }
