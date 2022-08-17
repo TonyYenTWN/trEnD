08474b9073d3e71aa9b03a5cab0ce3719cbed2e3
TonyYenTWN <tony.yen.uni.freiburg@gmail.com>
2022-08-17 14:52:45 +0200

    update documentation

--- docs/d5/d60/namespacepower__market.js
+++ docs/d5/d60/namespacepower__market.js
@@ -27,7 +27,7 @@ var namespacepower__market =
     [ "Source_Node_Set", "d5/d60/namespacepower__market.html#ae1e8fa9cb87172cb82028b0ddcf838d4", null ],
     [ "Submitted_bid_calculation", "d5/d60/namespacepower__market.html#ae8fbe0bb3bda42914047dd012a057f48", null ],
     [ "TSO_boundary_update", "d5/d60/namespacepower__market.html#af5b224f3d0c0c210ed0c66cc8140c5b1", null ],
-    [ "TSO_Market_control_reserve", "d5/d60/namespacepower__market.html#a68f11da2b5386a981687c4eb41303eb7", null ],
+    [ "TSO_Market_control_reserve", "d5/d60/namespacepower__market.html#a4aa93d7a810b01f7cb18754ab503577d", null ],
     [ "TSO_Market_Results_Get", "d5/d60/namespacepower__market.html#ac071507e193ed4c20579a682961bcd14", null ],
     [ "TSO_Market_Set", "d5/d60/namespacepower__market.html#adff40f938d2d4deba55dfc4779c01300", null ]
 ];
