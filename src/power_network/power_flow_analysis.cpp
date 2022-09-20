24bfc5396dac8d135340fb38d20309bf861215eb
TonyYenTWN <tony.yen.uni.freiburg@gmail.com>
2022-09-20 17:03:35 +0200

    Update power_flow_analysis.cpp

--- src/power_network/power_flow_analysis.cpp
+++ src/power_network/power_flow_analysis.cpp
@@ -3,11 +3,23 @@
 #include "src/power_market/power_market.h"
 
 // HELM method
-// P-Q Bus
+// P-Q buses
 // Set {V}(s) = \sum {a}_n * s^n
 // {\hat V}(s) = \sum {b}_n * s^n
-// {1 / V}(s) = \sum {c}_n * s^n
-// {1 / \hat V}(s) = \sum {d}_n * s^n
+// {1. / V}(s) = \sum {c}_n * s^n
+// {1. / \hat V}(s) = \sum {d}_n * s^n
 // Equations:
-// [Y] {V}(s) = s * {S*} . {1 / \hat V}(s)
+// [Y] {V}(s) = s * {Conj(S)} . {1. / \hat V}(s)
+// [Conj(Y)] {\hat V}(s) = s * {S} . {1. / V}(s)
+//
+// P-U buses
+// Set {a}, {b}, {c}, {d} the same as P-Q bus
+// {Q}(s) = \sum {e}_n * s^n
+// Equations:
+// [Y] {V}(s) = s * ({P} - j {Q}(s)) . {1. / \hat V}(s)
+// [Conj(Y)] {\hat V}(s) = s * ({P} + j {Q}(s)) . {1. / V}(s)
+// {V}(s) * {\hat V}(s) = {U_0^2} + s * ({U^2} - {U_0^2})
+//
+// Conservation law for currents
+// {1}^T [Y] {V}(s) = 0.
 
