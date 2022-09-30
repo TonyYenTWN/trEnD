db39d79994334d8cbfa05c066596d48b7abcb715
TonyYenTWN <tony.yen.uni.freiburg@gmail.com>
2022-09-30 03:36:47 +0200

    Create aggregator.h

---
+++ src/agent/aggregator.h
@@ -0,0 +1,23 @@
+// Header file for aggregators
+#pragma once
+#include "agent.h"
+
+namespace agent{
+	namespace aggregator{
+		namespace parameters{
+			static inline int foresight_time(){
+				int value = 24;
+				return value;
+			}
+		}
+
+		struct profile{
+			int point_ID;
+			Eigen::VectorXd price_expected_profile;
+			Eigen::VectorXd price_demand_profile;
+			Eigen::VectorXd price_supply_profile;
+		};
+
+		typedef std::vector <profile> profiles;
+	}
+}
