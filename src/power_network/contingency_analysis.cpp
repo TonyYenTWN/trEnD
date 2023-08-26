b24e399038569a3dd06375215a1f9df33a61d7b9
TonyYenTWN <tony.yen.uni.freiburg@gmail.com>
2023-08-26 22:38:10 +0200

    add contingency analysis header and cpp file

---
+++ src/power_network/contingency_analysis.cpp
@@ -0,0 +1,6 @@
+#include "src/basic/rw_csv.h"
+#include "src/power_market/power_market.h"
+
+//namespace local{
+//    void
+//}
---
+++ src/power_network/contingency_analysis.h
@@ -0,0 +1,4 @@
+// Header file for contingency analysis
+#pragma once
+
+#include "src/power_market/power_market.h"
